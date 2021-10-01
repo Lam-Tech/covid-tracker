969721a22a3f18a78a48ee3f37c876d3c41cdcdd
Your Name <yiwenc22@gmail.com>
2021-10-01 12:30:49 -1000

    Vaccinepage 50% finish need picture/data upload

---
+++ app/imports/api/stuff/Vaccine.js
@@ -0,0 +1,31 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+import { Tracker } from 'meteor/tracker';
+
+/**
+ * The VaccineCollection. It encapsulates state and variable values for stuff.
+ */
+class VaccineCollection {
+  constructor() {
+    this.name = 'VaccineCollection';
+    this.collection = new Mongo.Collection(this.name);
+
+    this.schema = new SimpleSchema({
+      name: { label: 'name', type: String },
+      vaccineType: {
+        type: String,
+        allowedValues: ['Pfizer', 'Moderna', 'Johnson & Johnson', 'AstraZeneca', 'Other'],
+      },
+      dose1Lot: { label: 'Manufacturer Lot Number', type: Number },
+      dose1Date: { label: 'Data Received', type: Date },
+      dose1Site: { label: 'Clinic Site', type: String },
+      dose2Lot: { label: 'Manufacturer Lot Number', type: Number },
+      dose2Date: { label: 'Data Received', type: Date },
+      dose2Site: { label: 'Clinic Site', type: String },
+    }, { tracker: Tracker });
+    this.collection.attachSchema(this.schema);
+    this.userPublicationName = `${this.name}.publication.user`;
+  }
+}
+
+export const Vaccine = new VaccineCollection();
--- app/imports/ui/components/VaccCard.jsx
+++ app/imports/ui/components/VaccCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Button, Icon, Image, Modal, Container, Form } from 'semantic-ui-react';
+import { Button, Icon, Image, Modal, Container, Header, ModalContent, Input, Select } from 'semantic-ui-react';
+import { Meteor } from 'meteor/meteor';
+import swal from 'sweetalert';
+import { Vaccine } from '../../api/stuff/Vaccine';
+import { Statuses } from '../../api/status/Status';
 
 const options = [
   { key: 'm', text: 'Moderna', value: 'Moderna' },
   { key: 'p', text: 'Pfizer', value: 'Pfizer' },
   { key: 'j', text: 'Johnson Johnson', value: 'Johnson Johnson' },
+  { key: 'a', text: 'AstraZeneca', value: 'AstraZeneca' },
+  { key: 'c', text: 'Other', value: 'Other' },
 ];
 
 /** A simple static component to render some text for the landing page. */
@@ -13,14 +19,38 @@ class VaccCard extends React.Component {
     super(props);
     this.state = {
       prompt: false,
+      name: this.name,
+      vaccineType: this.vaccineType,
+      dose1Lot: this.dose1Lot,
+      dose1Date: this.dose1Date,
+      does1Site: this.dose1Site,
+      dose2Lot: this.dose2Lot,
+      dose2Date: this.dose2Date,
+      does2Site: this.dose2Site,
     };
   }
 
   handleChange = (e, { value }) => this.setState({ value })
 
-  editUserStatus() {
+  editVaccine() {
+    if (true) {
+      Vaccine.collection.update(Meteor.userId, {
+        $set: {
+          name: this.name,
+          vaccineType: this.vaccineType,
+          dose1Lot: this.dose1Lot,
+          dose1Date: this.dose1Date,
+          does1Site: this.dose1Site,
+          dose2Lot: this.dose2Lot,
+          dose2Date: this.dose2Date,
+          does2Site: this.dose2Site,
+        },
+      },
+      (error) => (error ?
+        swal('Error', error.message, 'error') :
+        swal('Success', 'Status updated successfully', 'success')));
+    }
     this.setState({ prompt: false });
-
   }
 
   render() {
@@ -30,33 +60,65 @@ class VaccCard extends React.Component {
           onClose={() => this.setState({ prompt: false })}
           onOpen={() => this.setState({ prompt: true })}
           open={this.state.prompt}
-          size='large'
+          size='Large'
           trigger={<Button color='green' inverted>Edit Vaccine Card</Button>}
         >
           <Modal.Header size='huge'>
               Fill in the Vaccine information
           </Modal.Header>
-          <Modal.Content>
-            <Form>
-              <Form.Group widths='equal'>
-                <Form.Input placeholder='First name' label='First Name' />
-                <Form.Input placeholder='Last name' label='Last Name'/>
-                <Form.Select
-                  label='Type of Vaccine'
-                  options={options}
-                  placeholder='Vaccine'
-                />
-              </Form.Group>
-            </Form>
-          </Modal.Content>
-          <Modal.Description>
-            <Image size='medium' url='images/Placeholder.png' wrapped />
-          </Modal.Description>
+          <ModalContent image>
+            <Image size='Large' src='https://getstamped.co.uk/wp-content/uploads/WebsiteAssets/Placeholder.jpg'wrapped/>
+            <Modal.Description>
+              <Input
+                label='Name'
+                placeholder='Full Name'
+                value={this.name}
+              />
+              <Select
+                label='Vaccine Type'
+                placeholder='VaccineType'
+                options={options}
+                value={this.vaccineType}
+              />
+              <Header>First Does</Header>
+              <Input
+                label='Lot Number'
+                placeholder='1234567'
+                value={this.dose1Lot}
+              />
+              <Input
+                label='Data Received'
+                placeholder='mm/dd/yyyy'
+                value={this.dose1Date}
+              />
+              <Input
+                label='Clinic Site'
+                placeholder='queens,etc'
+                value={this.dose1Site}
+              />
+              <Header>Second Does</Header>
+              <Input
+                label='Lot Number'
+                placeholder='1234567'
+                value={this.dose2Lot}
+              />
+              <Input
+                label='Data Received'
+                placeholder='mm/dd/yyyy'
+                value={this.dose2Date}
+              />
+              <Input
+                label='Clinic Site'
+                placeholder='queens,etc'
+                value={this.dose2Site}
+              />
+            </Modal.Description>
+          </ModalContent>
           <Modal.Actions>
-            <Button color='red' inverted onClick={() => this.editUserStatus(false)}>
+            <Button color='red' inverted onClick={() => this.editVaccine(false)}>
               <Icon name='remove'/> Cancel
             </Button>
-            <Button color='green' inverted onClick={() => this.editUserStatus(true)}>
+            <Button color='green' inverted onClick={() => this.editVaccine(true)}>
               <Icon name='checkmark'/> Upload
             </Button>
           </Modal.Actions>
